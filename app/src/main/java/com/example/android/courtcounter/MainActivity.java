71ffb644b5b27cad8aa5c522e23408f9265df336
Isabel Palomar <isabel.palomarb@gmail.com>
2016-06-24 01:00:54 -0400

    Court Counter style changes, reset implementation

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -16,48 +16,83 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    /**
+     * Displays the score value for the team A
+     * @param score
+     */
+
     public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
     }
+
+    /**
+     * Displays the score for the team A
+     * @param score
+     */
 
     public void displayForTeamB(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
     }
 
+    /**
+     * Increase the score for Team A by 1 point
+     */
     public void addOneForTeamA(View view) {
         scoreTeamA = scoreTeamA + 1;
         displayForTeamA(scoreTeamA);
 
     }
-
+    /**
+     * Increase the score for Team A by 2 points
+     */
     public void addTwoForTeamA(View view) {
         scoreTeamA = scoreTeamA + 2;
         displayForTeamA(scoreTeamA);
     }
 
-
+    /**
+     * Increase the score for Team A by 3 points
+     */
     public void addThreeForTeamA(View view) {
         scoreTeamA = scoreTeamA + 3;
         displayForTeamA(scoreTeamA);
     }
 
+    /**
+     * Increase the score for Team B by 1 point
+     */
     public void addOneForTeamB(View view) {
         scoreTeamB = scoreTeamB + 1;
         displayForTeamB(scoreTeamB);
 
     }
 
+    /**
+     * Increase the score for Team B by 2 pointS
+     */
     public void addThreeForTeamB(View view) {
         scoreTeamB = scoreTeamB + 3;
         displayForTeamB(scoreTeamB);
     }
 
+    /**
+     * Increase the score for Team B by 3 points
+     */
     public void addTwoForTeamB(View view) {
         scoreTeamB = scoreTeamB + 2;
         displayForTeamB(scoreTeamB);
 
     }
+    /**
+     * Resets the score for team A and B to 0.
+     */
+    public void resetScore(View view) {
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
 
+    }
 }
